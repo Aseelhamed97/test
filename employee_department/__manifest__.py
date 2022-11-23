ac2c473ab4b5e59545a96f2e24512167b9a22ec5
Aseelhamed79 <aseel.hamed.it@gmail.com>
2022-11-23 10:00:13 +0000

    new edir

--- employee_department/__manifest__.py
+++ employee_department/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr.employee'],
+    'depends': ['base','res.partner'],
 
     # always loaded
     'data': [
