ed528099cd74f2caa059258ced9f25abe814cda4
Aseelhamed97 <aseel.hamed.it@gmail.com>
2022-11-23 13:47:14 +0000

    new edit

--- employee_test/models/.ipynb_checkpoints/models-checkpoint.py
+++ employee_test/models/.ipynb_checkpoints/models-checkpoint.py
@@ -2,6 +2,6 @@ from odoo import models, fields, api
 
 
 class employee_department(models.Model):
-    _inherit = 'hr.employee'
-    birthday = fields.Datetime("Date of birth")
+    _inherit = 'res.partner'
+    birthday = fields.Datetime('Date of birth')
    
