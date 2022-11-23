022097242b150ce6052f4eb346059b601a0a0932
Aseelhamed97 <aseel.hamed.it@gmail.com>
2022-11-23 09:25:42 +0000

    new edit

--- employee_department/models/models.py
+++ employee_department/models/models.py
@@ -4,5 +4,5 @@ from odoo import models, fields, api
 
 class employee_department(models.Model):
     _inherit = 'hr.employee'
-     birthday = fields.Datetime("Date of birth")
+    birthday = fields.Datetime("Date of birth")
    
